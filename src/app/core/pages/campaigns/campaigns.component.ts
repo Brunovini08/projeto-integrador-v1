788926d0a39d5a9cf7b19e39dfb771234a349e4f
EVELYN VAGNER <evelyn.vagner@etec.sp.gov.br>
2024-08-15 12:21:48 -0300

    alteração da tela de campanhas

--- src/app/core/pages/campaigns/campaigns.component.ts
+++ src/app/core/pages/campaigns/campaigns.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../../shared/components/navbar/navbar.component";
-
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
 @Component({
   selector: 'app-campaigns',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './campaigns.component.html',
   styleUrl: './campaigns.component.css'
 })
--- src/app/core/pages/hot-map/hot-map.component.ts
+++ src/app/core/pages/hot-map/hot-map.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
 import { LoginComponent } from "../login/login.component";
+import { FooterComponent } from "../../../shared/components/footer/footer.component";
 @Component({
   selector: 'app-hot-map',
   standalone: true,
-  imports: [NavbarComponent, LoginComponent],
+  imports: [NavbarComponent, LoginComponent, FooterComponent],
   templateUrl: './hot-map.component.html',
   styleUrl: './hot-map.component.css'
 })
